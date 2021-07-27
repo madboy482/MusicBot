3c91d5f0a7a6efe02afee7b619b039adfcfc724c
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-27 12:04:24 +0530

    Initial Commit

--- MusicBot/services/callsmusic/callsmusic.py
+++ MusicBot/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from DaisyXMusic.config import API_HASH, API_ID, SESSION_NAME
-from DaisyXMusic.services.callsmusic import queues
+from MusicBot.config import API_HASH, API_ID, SESSION_NAME
+from MusicBot.services.callsmusic import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
